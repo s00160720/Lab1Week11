71190f3973629856c7c17b91ad05368e64ea7f38
s00160720 <s00160720@mail.itsligo.ie>
2017-12-06 11:52:30 +0000

    Radlab work

---
+++ Lab1Week11/Migrations/ClubContext/201712061003414_ClubContextMigration.cs
@@ -0,0 +1,16 @@
+namespace Lab1Week11.Migrations.ClubContext
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ClubContextMigration : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
